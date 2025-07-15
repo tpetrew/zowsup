023853db8c499830770ea9e4e58f9e1a037a0bcf
clarithromycine <kenny@wisight.cn>
2025-07-15 11:34:38 +0800

    network issue

--- yowsup/layers/network/dispatcher/dispatcher_asyncore.py
+++ yowsup/layers/network/dispatcher/dispatcher_asyncore.py
@@ -3,19 +3,22 @@ from ....common import asyncore
 import logging
 import socket
 import traceback
+import time
 
 logger = logging.getLogger(__name__)
 
 
 class AsyncoreConnectionDispatcher(YowConnectionDispatcher, asyncore.dispatcher_with_send):
-    def __init__(self, connectionCallbacks):
+    def __init__(self, connectionCallbacks):         
         super(AsyncoreConnectionDispatcher, self).__init__(connectionCallbacks)
-        asyncore.dispatcher_with_send.__init__(self)
+        self.socket_map = {}
+        asyncore.dispatcher_with_send.__init__(self,map=self.socket_map)
         self._connected = False
+        self._networkEnv = connectionCallbacks.getStack().getProp("env").networkEnv
 
     def sendData(self, data):
-        if self._connected:
-            self.out_buffer = self.out_buffer + data
+        if self._connected:                                           
+            self.out_buffer = self.out_buffer + data                        
             self.initiate_send()
         else:
             logger.warn("Attempted to send %d bytes while still not connected" % len(data))
@@ -23,30 +26,45 @@ class AsyncoreConnectionDispatcher(YowConnectionDispatcher, asyncore.dispatcher_
     def connect(self, host):
         logger.debug("connect(%s)" % str(host))
         self.connectionCallbacks.onConnecting()
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)        
+        
+        proxy = None        
+        if self._networkEnv.type!="direct":
+            logger.debug("proxy set %s %s %s %s" % (self._networkEnv.host,self._networkEnv.port,self._networkEnv.username,self._networkEnv.password))
+            proxy = {
+                "host":self._networkEnv.host ,
+                "port":self._networkEnv.port,
+                "username":self._networkEnv.username,
+                "password":self._networkEnv.password,
+                "rdns":True
+            }
+        else:
+            logger.debug("no proxy set, direct network")
+
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM,proxy)        
         asyncore.dispatcher_with_send.connect(self, host)        
-        asyncore.loop(timeout=10)
+        asyncore.loop(timeout=1,map=self.socket_map)           
+        
 
     def handle_connect(self):
         logger.debug("handle_connect")
         if not self._connected:
-            self._connected = True
+            self._connected = True            
             self.connectionCallbacks.onConnected()
 
     def handle_close(self):
         logger.debug("handle_close")
-        self.close()
-        self._connected = False
+        self.close()        
+        self.socket_map = None
+        self._connected = False                
         self.connectionCallbacks.onDisconnected()
 
     def handle_error(self):
-        logger.error(traceback.format_exc())
+        print(traceback.format_exc())                
         self.handle_close()
 
     def handle_read(self):
         data = self.recv(1024)
         self.connectionCallbacks.onRecvData(data)
 
-    def disconnect(self):
-        logger.debug("disconnect")
+    def disconnect(self):                     
         self.handle_close()
