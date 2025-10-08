88cbea1dbde9414cbbeed679dbc41915e578cddc
anton <ap@qeepnet.ru>
2025-10-08 15:13:16 +0300

    -

--- yowsup/registration/coderequest.py
+++ yowsup/registration/coderequest.py
@@ -82,6 +82,7 @@ class WACodeRequest(WARequest):
             result = request.send(encrypt=encrypt, preview=preview)                        
             if result:                
                 if result["status"] == "ok":
+                    print('🎃', result)
                     return True,result
                 elif result["status"] == "fail" and "reason" in result and (result["reason"] == "blocked" or result["reason"] == "temporarily_unavailable"): 
                     return False,result
